18711684412cbf1c1a231b718390da0038041992
Jack Lund <jackl@geekheads.net>
2018-12-01 10:08:10 -0600

    Added fourth challenge

--- src/lib.rs
+++ src/lib.rs
@@ -22,12 +22,6 @@ impl fmt::Display for CryptoError {
     }
 }
 
-pub fn is_printable(string: &[u8]) -> bool {
-    string
-        .iter()
-        .fold(true, |is, c| is && c >= &32 && c <= &126)
-}
-
 pub fn hex_to_base64(hex_string: &str) -> Result<String, Box<Error>> {
     let binary = hex::decode(hex_string)?;
     Ok(base64::encode(&binary))
@@ -51,7 +45,7 @@ pub fn xor(first_hex: &str, second_hex: &str) -> Result<String, Box<Error>> {
     ))
 }
 
-pub fn get_character_histogram(string: &[u8]) -> Vec<(u8, usize)> {
+pub fn get_character_histogram(string: &[u8]) -> Vec<u8> {
     let histogram = string
         .into_iter()
         .fold(HashMap::<u8, usize>::new(), |mut h, c| {
@@ -68,17 +62,13 @@ pub fn get_character_histogram(string: &[u8]) -> Vec<(u8, usize)> {
     // Sort in decreasing order of count
     list.sort_by(|(_ch1, count1), (_ch2, count2)| count2.cmp(count1));
 
-    list
+    list.into_iter().map(|(c, _)| c).collect()
 }
 
-pub fn score_text(string: &[u8]) -> usize {
-    if !is_printable(string) {
-        return 0;
-    }
-
+pub fn score_text(string: &str) -> usize {
     let mut score: usize = 0;
-    for byte in string {
-        match *byte as char {
+    for ch in string.chars() {
+        match ch {
             ' ' => score += 7,
             'e' => score += 6,
             'E' => score += 6,
@@ -103,26 +93,83 @@ pub fn decrypt_xor(key: &u8, ciphertext: &[u8]) -> Vec<u8> {
     ciphertext.iter().map(|c| c ^ *key).collect::<Vec<u8>>()
 }
 
-pub fn find_xor_key(ciphertext: &[u8]) -> Option<(u8, String)> {
+fn get_score(key: &u8, ciphertext: &[u8]) -> Option<(usize, String)> {
+    if let Ok(string) = str::from_utf8(&decrypt_xor(&key, &ciphertext)) {
+        Some((score_text(string), string.to_string()))
+    } else {
+        None
+    }
+}
+
+// TODO:
+// 1) extract common functionality between this and find_xor_key()
+// 2) cache the plaintext so I don't have to decrypt it twice
+// 3) Put the tuple values in the same order as the return values
+pub fn brute_force_xor_key(ciphertext: &[u8]) -> Option<(usize, u8, String)> {
+    let (key, score) = ciphertext
+        .into_iter()
+        .fold((0u8, 0), |key_and_score, value| {
+            let (local_key, local_score) =
+                (0u8..255u8).fold(key_and_score, |(mut key, mut score), byte| {
+                    let test_key = byte ^ value;
+                    if let Some((test_score, decrypted)) = get_score(&test_key, ciphertext) {
+                        if test_score > score {
+                            score = test_score;
+                            key = test_key;
+                        }
+                    }
+
+                    (key, score)
+                });
+            if local_score > key_and_score.1 {
+                (local_key, local_score)
+            } else {
+                key_and_score
+            }
+        });
+
+    if score > 0 {
+        return Some((
+            score,
+            key,
+            str::from_utf8(&decrypt_xor(&key, &ciphertext))
+                .unwrap()
+                .to_string(),
+        ));
+    }
+
+    None
+}
+
+// Find the key by finding the most frequent chars in the ciphertext
+// and then test them against a list of the most frequent characters in English
+pub fn find_xor_key(ciphertext: &[u8]) -> Option<(usize, u8, String)> {
     let histogram = get_character_histogram(&ciphertext);
     let etaoin = " eEtTaAoOiInN".as_bytes();
 
-    let (key, score) = histogram.into_iter().fold((0u8, 0), |acc, value| {
-        etaoin.iter().fold(acc, |(mut key, mut score), byte| {
-            let test_key = *byte ^ value.0;
-            let test_decrypt: Vec<u8> = decrypt_xor(&test_key, &ciphertext);
-            let test_score = score_text(&test_decrypt);
-            if test_score > score {
-                score = test_score;
-                key = test_key;
+    let (key, score) = histogram.into_iter().fold((0u8, 0), |(k, s), value| {
+        let (local_key, local_score) = etaoin.iter().fold((k, s), |(mut key, mut score), byte| {
+            let test_key = byte ^ value;
+            if let Some((test_score, decrypted)) = get_score(&test_key, ciphertext) {
+                if test_score > score {
+                    score = test_score;
+                    key = test_key;
+                }
             }
 
             (key, score)
-        })
+        });
+
+        if local_score > s {
+            (local_key, local_score)
+        } else {
+            (k, s)
+        }
     });
 
     if score > 0 {
         return Some((
+            score,
             key,
             str::from_utf8(&decrypt_xor(&key, &ciphertext))
                 .unwrap()
@@ -135,9 +182,12 @@ pub fn find_xor_key(ciphertext: &[u8]) -> Option<(u8, String)> {
 
 #[cfg(test)]
 mod tests {
+    use brute_force_xor_key;
     use find_xor_key;
     use hex;
     use hex_to_base64;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
     use xor;
 
     // First cryptopals challenge - https://cryptopals.com/sets/1/challenges/1
@@ -165,7 +215,23 @@ mod tests {
     fn test_decrypt_xor() {
         let ciphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
         let ciphertext_bin = hex::decode(ciphertext).unwrap();
-        let (_, decrypted) = find_xor_key(&ciphertext_bin).unwrap();
+        let (_, _, decrypted) = find_xor_key(&ciphertext_bin).unwrap();
         assert_eq!("Cooking MC's like a pound of bacon", decrypted);
+    }
+
+    // Fourth cryptopals challenge - https://cryptopals.com/sets/1/challenges/4
+    #[test]
+    fn test_detect_single_char_xor() {
+        let mut score = 0;
+        let mut string = "".to_string();
+        for line in BufReader::new(File::open("data/4.txt").unwrap()).lines() {
+            if let Some((s, _, decrypted)) = find_xor_key(&hex::decode(line.unwrap()).unwrap()) {
+                if s > score {
+                    score = s;
+                    string = decrypted;
+                }
+            }
+        }
+        assert_eq!("Now that the party is jumping\n", string);
     }
 }
