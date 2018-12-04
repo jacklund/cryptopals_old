f7bde2a46acfd7b903c49e5d4bb07b021fde701a
Jack Lund <jackl@geekheads.net>
2018-12-04 14:08:26 -0600

    Added eighth challenge

--- src/lib.rs
+++ src/lib.rs
@@ -433,4 +433,33 @@ mod tests {
                 .starts_with("I'm back and I'm ringin' the bell")
         );
     }
+
+    // Eighth cryptopals challenge - https://cryptopals.com/sets/1/challenges/8
+    #[test]
+    fn test_detect_aes_in_ecb_mode() {
+        let mut found: Option<String> = None;
+        for line in BufReader::new(File::open("data/8.txt").unwrap()).lines() {
+            let real_line = line.unwrap();
+            let data = base64::decode(&real_line.clone()).unwrap();
+            let chunks = data.chunks(16)
+                .map(|c| c.to_vec())
+                .collect::<Vec<Vec<u8>>>();
+            for (index, chunk1) in chunks.iter().enumerate() {
+                for chunk2 in chunks[index + 1..].to_vec() {
+                    if *chunk1 == chunk2 {
+                        found = Some(real_line.clone());
+                        break;
+                    }
+                }
+                if found.is_some() {
+                    break;
+                }
+            }
+        }
+        assert!(
+            found
+                .unwrap()
+                .starts_with("d880619740a8a19b7840a8a31c810a3d08649")
+        );
+    }
 }
